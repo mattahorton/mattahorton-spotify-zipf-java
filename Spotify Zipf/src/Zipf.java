d9954abe80b856ee320dedc5411c760ab76e9423
mattahorton <matthew.a.horton@gmail.com>
2013-10-18 01:28:04 -0500

    Comments for readability

--- Spotify Zipf/src/Zipf.java
+++ Spotify Zipf/src/Zipf.java
@@ -12,9 +12,14 @@ public class Zipf {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Album Data Structure
 		ArrayList<Song> songs = new ArrayList<Song>();
+
+		//stdin and stdout setup
 		Scanner in = new Scanner(new BufferedInputStream(System.in));
 		PrintWriter writer = new PrintWriter(System.out);
+
+		//song properties
 		long fi, zi;
 		double qi;
 		String si;
@@ -24,13 +29,15 @@ public class Zipf {
 		int albumLength = in.nextInt();
 		int topTracksNum = in.nextInt();
 		
+		//Instantiate first track on album
 		fi = in.nextLong();
 		si = in.next();
 		zi = fi;
 		if(zi == 0) zi = 1;
-		qi = (double)fi;
+		qi = (double)fi; //first track is basis for album quality
 		songs.add(new Song(fi,zi,si,qi,trackCounter++));
 				
+		//instantiate song objects for rest of album
 		while(in.hasNext()){
 			fi = in.nextLong();
 			si = in.next();
@@ -39,16 +46,28 @@ public class Zipf {
 			songs.add(new Song(fi,zi,si,qi,trackCounter++));
 		}
 	
+		//Using the Comparable interface's compareTo() method, sort the album by quality
 		Collections.sort(songs);
 		
+
+		//Print out require number of top tracks
 		for(int i = 0; i < topTracksNum; i++){
 			writer.println(songs.get(i).getSi());
 		}
+
+		//flush and close writer
 		writer.flush();
 		writer.close();
 	}
 
+	/*
+	/ Song:
+	/ Implementation of the Comparable<T> interface
+	/ Takes advantage of compareTo() to be sorted by Collections.sort()
+	*/
+
 	public static class Song implements Comparable<Song>{
+		//Song property instance variables
 		private long fi;
 		private long zi;
 		private String si;
@@ -65,7 +84,12 @@ public class Zipf {
 
 		@Override
 		public int compareTo(Song other) {
-			// TODO Auto-generated method stub
+
+			// returns 1 if quality is greater than the quality of the other object.
+			// returns -1 if quality is less than the quality of the other object.
+			// returns -1 if tracks have same quality and the current track is 
+			// an earlier track on the album.
+
 			if(qi < other.getQi()){
 				return 1;
 			}
@@ -79,6 +103,8 @@ public class Zipf {
 			}
 			return 1;
 		}
+
+		//Getters and Setters
 
 		public long getFi() {
 			return fi;
